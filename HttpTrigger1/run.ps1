f9b7a3b82308442fcda4d74aea537854dbb01ea2
mauricioOC <mauricio.ortiz@live.com>
2021-09-29 17:59:11 +0000

    2

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -15,7 +15,7 @@ if (-not $name) {
 $body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
 
 if ($name) {
-    $body = "Hello, $name. Today is $date and this HTTP triggered function executed successfully. Jesusiñños"
+    $body = "Hello, $name. Today is $date and this HTTP triggered function executed successfully. Jesusiñños 2"
 }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
